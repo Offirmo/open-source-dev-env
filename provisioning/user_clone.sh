259c0186ba677963a18aa35e3ad407712919aecc
Offirmo <offirmo.net@gmail.com>
2016-10-19 20:28:42 +0200

    +

--- provisioning/user_clone.sh
+++ provisioning/user_clone.sh
@@ -33,16 +33,16 @@ echo "  - PATH          = $PATH"
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/Etendard.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/hello-world-npm.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/internationalize.git
-~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/minisite-w.git
+~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/minisite-w.git             m-w
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/node-typescript-compiler.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/offirmo.github.io.git
-~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/online-adventur.es.git   oa
+~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/online-adventur.es.git     oa
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/prefixed-log.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/random-js.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/simplyconfig.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/sync-local-repos.git
-~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/tbrpg-model.git        tb-model
-~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/tbrpg-static-data.git  tb-static-data
+~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/tbrpg-model.git            tb-model
+~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/tbrpg-static-data.git      tb-static-data
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/unit-test-toolbox.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/web-tech-experiments.git
 ~/work/src/virtualized-open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo-team/wiki.wiki.git
