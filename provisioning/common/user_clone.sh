f9bd587d9ec2fa84c9c0cc29b411fd632144ab39
Offirmo <offirmo.net@gmail.com>
2017-01-29 22:24:11 +0700

    +

--- provisioning/common/user_clone.sh
+++ provisioning/common/user_clone.sh
@@ -20,24 +20,27 @@ echo "  - PATH          = $PATH"
 
 
 ############ Repos ############
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/bangja.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/clean-code-toolbox.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/cli-toolbox.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/cross-js-env-tests.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/Etendard.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/hello-world-npm.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/internationalize.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/minisite-bookmarks.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/minisite-w.git             m-w
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/node-typescript-compiler.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/offirmo.github.io.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/online-adventur.es.git     oa
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/prefixed-log.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/random-js.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/simplyconfig.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/sync-local-repos.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/tbrpg-model.git            tb-model
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/tbrpg-static-data.git      tb-static-data
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/unit-test-toolbox.git
-~/work/src/open-source-dev-env/bin/gitc.sh  git@github.com:Offirmo/web-tech-experiments.git
+## git@github.com:
+## https://github.com/
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/bangja.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/clean-code-toolbox.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/cli-toolbox.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/cross-js-env-tests.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/Etendard.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/hello-world-npm.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/internationalize.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/minisite-bookmarks.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/minisite-w.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/node-typescript-compiler.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/offirmo.github.io.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/online-adventur.es.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/prefixed-log.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/random-js.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/simplyconfig.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/sync-local-repos.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/tbrpg-model.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/tbrpg-static-data.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/unit-test-toolbox.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/web-tech-experiments.git
+~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo/web-module-toolbox.git
 ~/work/src/open-source-dev-env/bin/gitc.sh  https://github.com/Offirmo-team/wiki.wiki.git
