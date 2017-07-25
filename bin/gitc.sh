6ce2fd9ee76eb706b46bd67499b35ed978ce6a46
Offirmo <offirmo.net@gmail.com>
2017-07-25 19:52:05 +1000

    update

--- bin/gitc.sh
+++ bin/gitc.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
 REPOSITORY_URL=$1
-DEFAULT_DIR="$(basename "$REPOSITORY_URL")"
-DEFAULT_DIR=${DEFAULT_DIR%".git"}
-CUSTOM_DIR=$2
+CUSTOM_REPO_DIR=$2
 
-if [[ -n "$CUSTOM_DIR" ]]; then
-    TARGET_DIR="$CUSTOM_DIR"
+DEFAULT_PARENT_DIR=~/work/src
+DEFAULT_REPO_DIR="$(basename "$REPOSITORY_URL")"
+DEFAULT_REPO_DIR=${DEFAULT_REPO_DIR%".git"}
+
+PARENT_DIR="$DEFAULT_PARENT_DIR"
+
+if [[ -n "$CUSTOM_REPO_DIR" ]]; then
+    TARGET_DIR="$CUSTOM_REPO_DIR"
 else
-    TARGET_DIR="$DEFAULT_DIR"
+    TARGET_DIR="$DEFAULT_REPO_DIR"
 fi
 
 
-echo "* cloning $REPOSITORY_URL into ~/work/src/${TARGET_DIR}..."
+echo "* cloning $REPOSITORY_URL into ${PARENT_DIR}/${TARGET_DIR}..."
 
-pushd ~/work/src > /dev/null
+pushd $PARENT_DIR > /dev/null
 
 if [[ ! -d $TARGET_DIR ]]; then
     git clone --recursive "$REPOSITORY_URL" "$TARGET_DIR"
