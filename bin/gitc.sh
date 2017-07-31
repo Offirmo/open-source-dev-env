c6dd25d0dff743983360a0ac0e34f237d06c729b
Offirmo <offirmo.net@gmail.com>
2017-07-31 13:59:17 +1000

    TLDW :scream_cat:

--- bin/gitc.sh
+++ bin/gitc.sh
@@ -3,11 +3,25 @@
 REPOSITORY_URL=$1
 CUSTOM_REPO_DIR=$2
 
+# strips trailing / if any
+REPOSITORY_URL=${REPOSITORY_URL%"/"}
+
 DEFAULT_PARENT_DIR=~/work/src
-DEFAULT_REPO_DIR="$(basename "$REPOSITORY_URL")"
-DEFAULT_REPO_DIR=${DEFAULT_REPO_DIR%".git"}
+LAST_URL_SEGMENT="$(basename "$REPOSITORY_URL")"
+DEFAULT_REPO_DIR=${LAST_URL_SEGMENT%".git"}
 
 PARENT_DIR="$DEFAULT_PARENT_DIR"
+# strips last segment from the full url
+TEMP=${REPOSITORY_URL%"/$LAST_URL_SEGMENT"}
+# then get n-1 segment
+TEMP="$(basename "$TEMP")"
+
+# if recognized as an expected subdir, change parent dir
+if [[ $TEMP = "offirmo" ]]; then
+    PARENT_DIR=$PARENT_DIR/$TEMP
+elif [[ $TEMP = "online-adventures" ]]; then
+    PARENT_DIR=$PARENT_DIR/$TEMP
+fi
 
 if [[ -n "$CUSTOM_REPO_DIR" ]]; then
     TARGET_DIR="$CUSTOM_REPO_DIR"
@@ -18,6 +32,7 @@ fi
 
 echo "* cloning $REPOSITORY_URL into ${PARENT_DIR}/${TARGET_DIR}..."
 
+mkdir -p $PARENT_DIR
 pushd $PARENT_DIR > /dev/null
 
 if [[ ! -d $TARGET_DIR ]]; then
